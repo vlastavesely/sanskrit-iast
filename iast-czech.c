ce3a02ad6efa524f3d87afad246ea8799bd6eff4
Vlasta Vesely <vlastavesely@protonmail.ch>
2018-05-03 19:53:14 +0200

    count all the letters

--- iast-czech.c
+++ iast-czech.c
@@ -13,55 +13,55 @@
 static const struct transliteration_letter table[] = {
 
 	/* Vowels */
-	{0x0905, FLAG_REGULAR, "a"},		/* अ */
-	{0x0906, FLAG_REGULAR, "á"},		/* आ */
-	{0x0907, FLAG_REGULAR, "i"},		/* इ */
-	{0x0908, FLAG_REGULAR, "í"},		/* ई */
-	{0x0909, FLAG_REGULAR, "u"},		/* उ */
-	{0x090a, FLAG_REGULAR, "ú"},		/* ऊ */
-	{0x090b, FLAG_REGULAR, "r"},		/* ऋ */
-	{0x0960, FLAG_REGULAR, "r"},		/* ॠ */
-	{0x090c, FLAG_REGULAR, "l"},		/* ऌ */
-	{0x0961, FLAG_REGULAR, "l"},		/* ॡ */
-	{0x090f, FLAG_REGULAR, "é"},		/* ए */
-	{0x0910, FLAG_REGULAR, "ai"},		/* ऐ */
-	{0x0913, FLAG_REGULAR, "ó"},		/* ओ */
-	{0x0914, FLAG_REGULAR, "au"},		/* औ */
+	{0x0905, FLAG_REGULAR, "a"},		/* अ */		/* 1 */
+	{0x0906, FLAG_REGULAR, "á"},		/* आ */		/* 2 */
+	{0x0907, FLAG_REGULAR, "i"},		/* इ */		/* 3 */
+	{0x0908, FLAG_REGULAR, "í"},		/* ई */		/* 4 */
+	{0x0909, FLAG_REGULAR, "u"},		/* उ */		/* 5 */
+	{0x090a, FLAG_REGULAR, "ú"},		/* ऊ */		/* 6 */
+	{0x090b, FLAG_REGULAR, "r"},		/* ऋ */		/* 7 */
+	{0x0960, FLAG_REGULAR, "r"},		/* ॠ */		/* 8 */
+	{0x090c, FLAG_REGULAR, "l"},		/* ऌ */		/* 9 */
+	{0x0961, FLAG_REGULAR, "l"},		/* ॡ */		/* 10 */
+	{0x090f, FLAG_REGULAR, "é"},		/* ए */		/* 11 */
+	{0x0910, FLAG_REGULAR, "ai"},		/* ऐ */		/* 12 */
+	{0x0913, FLAG_REGULAR, "ó"},		/* ओ */		/* 13 */
+	{0x0914, FLAG_REGULAR, "au"},		/* औ */		/* 14 */
 
 	/* Consonants */
-	{0x0915, FLAG_REGULAR, "ka"},		/* क */
-	{0x0916, FLAG_REGULAR, "kha"},		/* ख */
-	{0x0917, FLAG_REGULAR, "ga"},		/* ग */
-	{0x0918, FLAG_REGULAR, "gha"},		/* घ */
-	{0x0919, FLAG_REGULAR, "na"},		/* ङ */
-	{0x0939, FLAG_REGULAR, "ha"},		/* ह */
-	{0x091a, FLAG_REGULAR, "ca"},		/* च */
-	{0x091b, FLAG_REGULAR, "cha"},		/* छ */
-	{0x091c, FLAG_REGULAR, "dža"},		/* ज */
-	{0x091d, FLAG_REGULAR, "džha"},		/* झ */
-	{0x091e, FLAG_REGULAR, "ňa"},		/* ञ */
-	{0x092f, FLAG_REGULAR, "ja"},		/* य */
-	{0x0936, FLAG_REGULAR, "ša"},		/* श */
-	{0x091F, FLAG_REGULAR, "ta"},		/* ट */
-	{0x0920, FLAG_REGULAR, "tha"},		/* ठ */
-	{0x0921, FLAG_REGULAR, "da"},		/* ड */
-	{0x0922, FLAG_REGULAR, "dha"},		/* ढ */
-	{0x0923, FLAG_REGULAR, "na"},		/* ण */
-	{0x0930, FLAG_REGULAR, "ra"},		/* र */
-	{0x0937, FLAG_REGULAR, "ša"},		/* श */
-	{0x0924, FLAG_REGULAR, "ta"},		/* त */
-	{0x0925, FLAG_REGULAR, "tha"},		/* थ */
-	{0x0926, FLAG_REGULAR, "da"},		/* द */
-	{0x0927, FLAG_REGULAR, "dha"},		/* ध */
-	{0x0928, FLAG_REGULAR, "na"},		/* न */
-	{0x0932, FLAG_REGULAR, "la"},		/* ल */
-	{0x0938, FLAG_REGULAR, "sa"},		/* स */
-	{0x092a, FLAG_REGULAR, "pa"},		/* प */
-	{0x092b, FLAG_REGULAR, "pha"},		/* फ */
-	{0x092c, FLAG_REGULAR, "ba"},		/* ब */
-	{0x092d, FLAG_REGULAR, "bha"},		/* भ */
-	{0x092e, FLAG_REGULAR, "ma"},		/* म */
-	{0x0935, FLAG_REGULAR, "va"},		/* व */
+	{0x0915, FLAG_REGULAR, "ka"},		/* क */		/* 1 */
+	{0x0916, FLAG_REGULAR, "kha"},		/* ख */		/* 2 */
+	{0x0917, FLAG_REGULAR, "ga"},		/* ग */		/* 3 */
+	{0x0918, FLAG_REGULAR, "gha"},		/* घ */		/* 4 */
+	{0x0919, FLAG_REGULAR, "na"},		/* ङ */		/* 5 */
+	{0x0939, FLAG_REGULAR, "ha"},		/* ह */		/* 6 */
+	{0x091a, FLAG_REGULAR, "ca"},		/* च */		/* 7 */
+	{0x091b, FLAG_REGULAR, "cha"},		/* छ */		/* 8 */
+	{0x091c, FLAG_REGULAR, "dža"},		/* ज */		/* 9 */
+	{0x091d, FLAG_REGULAR, "džha"},		/* झ */		/* 10 */
+	{0x091e, FLAG_REGULAR, "ňa"},		/* ञ */		/* 11 */
+	{0x092f, FLAG_REGULAR, "ja"},		/* य */		/* 12 */
+	{0x0936, FLAG_REGULAR, "ša"},		/* श */		/* 13 */
+	{0x091F, FLAG_REGULAR, "ta"},		/* ट */		/* 14 */
+	{0x0920, FLAG_REGULAR, "tha"},		/* ठ */		/* 15 */
+	{0x0921, FLAG_REGULAR, "da"},		/* ड */		/* 16 */
+	{0x0922, FLAG_REGULAR, "dha"},		/* ढ */		/* 17 */
+	{0x0923, FLAG_REGULAR, "na"},		/* ण */		/* 18 */
+	{0x0930, FLAG_REGULAR, "ra"},		/* र */		/* 19 */
+	{0x0937, FLAG_REGULAR, "ša"},		/* श */		/* 20 */
+	{0x0924, FLAG_REGULAR, "ta"},		/* त */		/* 21 */
+	{0x0925, FLAG_REGULAR, "tha"},		/* थ */		/* 22 */
+	{0x0926, FLAG_REGULAR, "da"},		/* द */		/* 23 */
+	{0x0927, FLAG_REGULAR, "dha"},		/* ध */		/* 24 */
+	{0x0928, FLAG_REGULAR, "na"},		/* न */		/* 25 */
+	{0x0932, FLAG_REGULAR, "la"},		/* ल */		/* 26 */
+	{0x0938, FLAG_REGULAR, "sa"},		/* स */		/* 27 */
+	{0x092a, FLAG_REGULAR, "pa"},		/* प */		/* 28 */
+	{0x092b, FLAG_REGULAR, "pha"},		/* फ */		/* 29 */
+	{0x092c, FLAG_REGULAR, "ba"},		/* ब */		/* 30 */
+	{0x092d, FLAG_REGULAR, "bha"},		/* भ */		/* 31 */
+	{0x092e, FLAG_REGULAR, "ma"},		/* म */		/* 32 */
+	{0x0935, FLAG_REGULAR, "va"},		/* व */		/* 33 */
 
 	/* Codas */
 	{0x0902, FLAG_REGULAR, "m"},		/* ं (anusvara) */
--- iast.c
+++ iast.c
@@ -10,55 +10,55 @@
 static const struct transliteration_letter table[] = {
 
 	/* Vowels */
-	{0x0905, FLAG_REGULAR, "a"},		/* अ */
-	{0x0906, FLAG_REGULAR, "ā"},		/* आ */
-	{0x0907, FLAG_REGULAR, "i"},		/* इ */
-	{0x0908, FLAG_REGULAR, "ī"},		/* ई */
-	{0x0909, FLAG_REGULAR, "u"},		/* उ */
-	{0x090a, FLAG_REGULAR, "ū"},		/* ऊ */
-	{0x090b, FLAG_REGULAR, "ṛ"},		/* ऋ */
-	{0x0960, FLAG_REGULAR, "ṝ"},		/* ॠ */
-	{0x090c, FLAG_REGULAR, "ḷ"},		/* ऌ */
-	{0x0961, FLAG_REGULAR, "ḹ"},		/* ॡ */
-	{0x090f, FLAG_REGULAR, "e"},		/* ए */
-	{0x0910, FLAG_REGULAR, "ai"},		/* ऐ */
-	{0x0913, FLAG_REGULAR, "o"},		/* ओ */
-	{0x0914, FLAG_REGULAR, "au"},		/* औ */
+	{0x0905, FLAG_REGULAR, "a"},		/* अ */		/* 1 */
+	{0x0906, FLAG_REGULAR, "ā"},		/* आ */		/* 2 */
+	{0x0907, FLAG_REGULAR, "i"},		/* इ */		/* 3 */
+	{0x0908, FLAG_REGULAR, "ī"},		/* ई */		/* 4 */
+	{0x0909, FLAG_REGULAR, "u"},		/* उ */		/* 5 */
+	{0x090a, FLAG_REGULAR, "ū"},		/* ऊ */		/* 6 */
+	{0x090b, FLAG_REGULAR, "ṛ"},		/* ऋ */		/* 7 */
+	{0x0960, FLAG_REGULAR, "ṝ"},		/* ॠ */		/* 8 */
+	{0x090c, FLAG_REGULAR, "ḷ"},		/* ऌ */		/* 9 */
+	{0x0961, FLAG_REGULAR, "ḹ"},		/* ॡ */		/* 10 */
+	{0x090f, FLAG_REGULAR, "e"},		/* ए */		/* 11 */
+	{0x0910, FLAG_REGULAR, "ai"},		/* ऐ */		/* 12 */
+	{0x0913, FLAG_REGULAR, "o"},		/* ओ */		/* 13 */
+	{0x0914, FLAG_REGULAR, "au"},		/* औ */		/* 14 */
 
 	/* Consonants */
-	{0x0915, FLAG_REGULAR, "ka"},		/* क */
-	{0x0916, FLAG_REGULAR, "kha"},		/* ख */
-	{0x0917, FLAG_REGULAR, "ga"},		/* ग */
-	{0x0918, FLAG_REGULAR, "gha"},		/* घ */
-	{0x0919, FLAG_REGULAR, "ṅa"},		/* ङ */
-	{0x0939, FLAG_REGULAR, "ha"},		/* ह */
-	{0x091a, FLAG_REGULAR, "ca"},		/* च */
-	{0x091b, FLAG_REGULAR, "cha"},		/* छ */
-	{0x091c, FLAG_REGULAR, "ja"},		/* ज */
-	{0x091d, FLAG_REGULAR, "jha"},		/* झ */
-	{0x091e, FLAG_REGULAR, "ña"},		/* ञ */
-	{0x092f, FLAG_REGULAR, "ya"},		/* य */
-	{0x0936, FLAG_REGULAR, "śa"},		/* श */
-	{0x091F, FLAG_REGULAR, "ṭa"},		/* ट */
-	{0x0920, FLAG_REGULAR, "ṭha"},		/* ठ */
-	{0x0921, FLAG_REGULAR, "ḍa"},		/* ड */
-	{0x0922, FLAG_REGULAR, "ḍha"},		/* ढ */
-	{0x0923, FLAG_REGULAR, "ṇa"},		/* ण */
-	{0x0930, FLAG_REGULAR, "ra"},		/* र */
-	{0x0937, FLAG_REGULAR, "ṣa"},		/* श */
-	{0x0924, FLAG_REGULAR, "ta"},		/* त */
-	{0x0925, FLAG_REGULAR, "tha"},		/* थ */
-	{0x0926, FLAG_REGULAR, "da"},		/* द */
-	{0x0927, FLAG_REGULAR, "dha"},		/* ध */
-	{0x0928, FLAG_REGULAR, "na"},		/* न */
-	{0x0932, FLAG_REGULAR, "la"},		/* ल */
-	{0x0938, FLAG_REGULAR, "sa"},		/* स */
-	{0x092a, FLAG_REGULAR, "pa"},		/* प */
-	{0x092b, FLAG_REGULAR, "pha"},		/* फ */
-	{0x092c, FLAG_REGULAR, "ba"},		/* ब */
-	{0x092d, FLAG_REGULAR, "bha"},		/* भ */
-	{0x092e, FLAG_REGULAR, "ma"},		/* म */
-	{0x0935, FLAG_REGULAR, "va"},		/* व */
+	{0x0915, FLAG_REGULAR, "ka"},		/* क */		/* 1 */
+	{0x0916, FLAG_REGULAR, "kha"},		/* ख */		/* 2 */
+	{0x0917, FLAG_REGULAR, "ga"},		/* ग */		/* 3 */
+	{0x0918, FLAG_REGULAR, "gha"},		/* घ */		/* 4 */
+	{0x0919, FLAG_REGULAR, "ṅa"},		/* ङ */		/* 5 */
+	{0x0939, FLAG_REGULAR, "ha"},		/* ह */		/* 6 */
+	{0x091a, FLAG_REGULAR, "ca"},		/* च */		/* 7 */
+	{0x091b, FLAG_REGULAR, "cha"},		/* छ */		/* 8 */
+	{0x091c, FLAG_REGULAR, "ja"},		/* ज */		/* 9 */
+	{0x091d, FLAG_REGULAR, "jha"},		/* झ */		/* 10 */
+	{0x091e, FLAG_REGULAR, "ña"},		/* ञ */		/* 11 */
+	{0x092f, FLAG_REGULAR, "ya"},		/* य */		/* 12 */
+	{0x0936, FLAG_REGULAR, "śa"},		/* श */		/* 13 */
+	{0x091F, FLAG_REGULAR, "ṭa"},		/* ट */		/* 14 */
+	{0x0920, FLAG_REGULAR, "ṭha"},		/* ठ */		/* 15 */
+	{0x0921, FLAG_REGULAR, "ḍa"},		/* ड */		/* 16 */
+	{0x0922, FLAG_REGULAR, "ḍha"},		/* ढ */		/* 17 */
+	{0x0923, FLAG_REGULAR, "ṇa"},		/* ण */		/* 18 */
+	{0x0930, FLAG_REGULAR, "ra"},		/* र */		/* 19 */
+	{0x0937, FLAG_REGULAR, "ṣa"},		/* श */		/* 20 */
+	{0x0924, FLAG_REGULAR, "ta"},		/* त */		/* 21 */
+	{0x0925, FLAG_REGULAR, "tha"},		/* थ */		/* 22 */
+	{0x0926, FLAG_REGULAR, "da"},		/* द */		/* 23 */
+	{0x0927, FLAG_REGULAR, "dha"},		/* ध */		/* 24 */
+	{0x0928, FLAG_REGULAR, "na"},		/* न */		/* 25 */
+	{0x0932, FLAG_REGULAR, "la"},		/* ल */		/* 26 */
+	{0x0938, FLAG_REGULAR, "sa"},		/* स */		/* 27 */
+	{0x092a, FLAG_REGULAR, "pa"},		/* प */		/* 28 */
+	{0x092b, FLAG_REGULAR, "pha"},		/* फ */		/* 29 */
+	{0x092c, FLAG_REGULAR, "ba"},		/* ब */		/* 30 */
+	{0x092d, FLAG_REGULAR, "bha"},		/* भ */		/* 31 */
+	{0x092e, FLAG_REGULAR, "ma"},		/* म */		/* 32 */
+	{0x0935, FLAG_REGULAR, "va"},		/* व */		/* 33 */
 
 	/* Codas */
 	{0x0902, FLAG_REGULAR, "ṃ"},		/* ं (anusvara) */
