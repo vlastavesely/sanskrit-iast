5f68c20f83771759db807164bdf73cbd67e66ffc
Vlasta Vesely <vlastavesely@protonmail.ch>
2020-10-16 09:21:11 +0200

    update docs

--- iast.c
+++ iast.c
@@ -13,15 +13,15 @@ static struct translit_letter table[] = {
 	{0x0910, VOWEL, "ai"},           /* 01 */
 	{0x0914, VOWEL, "au"},           /* 02 */
 	{0x0905, VOWEL, "a"},            /* 03 */
-	{0x0906, VOWEL, "\u0101"},       /* 04 (a-) */
+	{0x0906, VOWEL, "\u0101"},       /* 04 (aa) */
 	{0x0907, VOWEL, "i"},            /* 05 */
-	{0x0908, VOWEL, "\u012b"},       /* 06 (i-) */
+	{0x0908, VOWEL, "\u012b"},       /* 06 (ii) */
 	{0x0909, VOWEL, "u"},            /* 07 */
-	{0x090a, VOWEL, "\u016b"},       /* 08 (u-) */
+	{0x090a, VOWEL, "\u016b"},       /* 08 (uu) */
 	{0x090b, VOWEL, "\u1e5b"},       /* 09 (.r) */
-	{0x0960, VOWEL, "\u1e5d"},       /* 10 (.r-) */
+	{0x0960, VOWEL, "\u1e5d"},       /* 10 (.rr) */
 	{0x090c, VOWEL, "\u1e37"},       /* 11 (.l) */
-	{0x0961, VOWEL, "\u1e39"},       /* 12 (.l-) */
+	{0x0961, VOWEL, "\u1e39"},       /* 12 (.ll) */
 	{0x090f, VOWEL, "e"},            /* 13 */
 	{0x0913, VOWEL, "o"},            /* 14 */
 
@@ -38,13 +38,13 @@ static struct translit_letter table[] = {
 	{0x092d, CONSONANT, "bh"},       /* 10 */
 	{0x0915, CONSONANT, "k"},        /* 11 */
 	{0x0917, CONSONANT, "g"},        /* 12 */
-	{0x0919, CONSONANT, "\u1e45"},   /* 13 (n.) */
+	{0x0919, CONSONANT, "\u1e45"},   /* 13 ("n) */
 	{0x0939, CONSONANT, "h"},        /* 14 */
 	{0x091a, CONSONANT, "c"},        /* 15 */
 	{0x091c, CONSONANT, "j"},        /* 16 */
 	{0x091e, CONSONANT, "\u00f1"},   /* 17 (n~) */
 	{0x092f, CONSONANT, "y"},        /* 18 */
-	{0x0936, CONSONANT, "\u015b"},   /* 19 (s,) */
+	{0x0936, CONSONANT, "\u015b"},   /* 19 ("s) */
 	{0x091f, CONSONANT, "\u1e6d"},   /* 20 (.t) */
 	{0x0921, CONSONANT, "\u1e0d"},   /* 21 (.d) */
 	{0x0923, CONSONANT, "\u1e47"},   /* 22 (.n) */
@@ -82,15 +82,15 @@ static struct translit_letter table[] = {
 	/* Diacritic modifiers */
 	{0x0948, VOWEL_SIGN, "ai"},
 	{0x094c, VOWEL_SIGN, "au"},
-	{0x093e, VOWEL_SIGN, "\u0101"},  /* (a-) */
+	{0x093e, VOWEL_SIGN, "\u0101"},  /* (aa) */
 	{0x093f, VOWEL_SIGN, "i"},
-	{0x0940, VOWEL_SIGN, "\u012b"},  /* (i-) */
+	{0x0940, VOWEL_SIGN, "\u012b"},  /* (ii) */
 	{0x0941, VOWEL_SIGN, "u"},
-	{0x0942, VOWEL_SIGN, "\u016b"},  /* (u-) */
+	{0x0942, VOWEL_SIGN, "\u016b"},  /* (uu) */
 	{0x0943, VOWEL_SIGN, "\u1e5b"},  /* (.r) */
-	{0x0944, VOWEL_SIGN, "\u1e5d"},  /* (.r-) */
+	{0x0944, VOWEL_SIGN, "\u1e5d"},  /* (.rr) */
 	{0x0962, VOWEL_SIGN, "\u1e37"},  /* (.l) */
-	{0x0963, VOWEL_SIGN, "\u1e39"},  /* (.l-) */
+	{0x0963, VOWEL_SIGN, "\u1e39"},  /* (.ll) */
 	{0x0947, VOWEL_SIGN, "e"},
 	{0x094b, VOWEL_SIGN, "o"},
 	{0x094d, VOWEL_SIGN, ""},        /* virama */
