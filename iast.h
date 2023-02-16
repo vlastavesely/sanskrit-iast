f63901f0cc895078e6abdfb465c8e52f9db967cc
Vlasta Vesely <vlastavesely@protonmail.ch>
2023-02-16 08:55:55 +0100

    compile libiast.so

---
+++ iast.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __IAST_H
+#define __IAST_H
+
+#include <iast/transliteration.h>
+#include <iast/velthuis.h>
+#include <iast/harvard-kyoto.h>
+
+#endif /* __IAST_H */
